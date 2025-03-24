d7c63cd5c358f444991cbbc8b13f60db267370cc
Darren0724 <93816310+Darren0724@users.noreply.github.com>
2025-03-24 14:27:33 +0800

    1

--- student_agent.py
+++ student_agent.py
@@ -63,6 +63,8 @@ def get_action(obs, reward=None, next_obs=None):
     
     global now_doing, goal_r, goal_c, now_r, now_c, row, col, st, ed, last_action, q_table, move_history, rec_reward, rec_state, step, epsilon
     step += 1
+    if step > 100:
+        epsilon = 0.05
     if step > 200:
         epsilon = 0.15
     if step > 400:
